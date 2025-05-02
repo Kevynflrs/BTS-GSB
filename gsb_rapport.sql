79426fde7a4e75f10534b94dbeac6a6188d7527f
Kevynflrs <k.floresdacosta@ecoles-epsi.net>
2025-05-02 11:47:16 +0200

    amélioration du code, de la bdd ajout de select avec toute les région

--- gsb_rapport.sql
+++ gsb_rapport.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.2-1.fc41
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 31 mars 2025 à 08:56
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Hôte : localhost
+-- Généré le : ven. 02 mai 2025 à 08:08
+-- Version du serveur : 10.11.11-MariaDB
+-- Version de PHP : 8.3.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,48 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `dirige`
---
-
-DROP TABLE IF EXISTS `dirige`;
-CREATE TABLE IF NOT EXISTS `dirige` (
-  `Id_Délégué` int NOT NULL,
-  `Id_Responsable` int NOT NULL,
-  PRIMARY KEY (`Id_Délégué`,`Id_Responsable`),
-  KEY `Id_Responsable` (`Id_Responsable`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `délégué`
---
-
-DROP TABLE IF EXISTS `délégué`;
-CREATE TABLE IF NOT EXISTS `délégué` (
-  `Id_Délégué` int NOT NULL AUTO_INCREMENT,
-  `IdRegion` int NOT NULL,
-  `Id_Utilisateur` int NOT NULL,
-  PRIMARY KEY (`Id_Délégué`),
-  KEY `IdRegion` (`IdRegion`),
-  KEY `Id_Utilisateur` (`Id_Utilisateur`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `echantillon`
 --
 
-DROP TABLE IF EXISTS `echantillon`;
-CREATE TABLE IF NOT EXISTS `echantillon` (
-  `Id_Echantillon` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `echantillon` (
+  `Id_Echantillon` int(11) NOT NULL,
   `DateDistributionEchantillon` date DEFAULT NULL,
-  `NomEchantillon` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `Libele` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `QuantiteEchantillon` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`Id_Echantillon`)
+  `NomEchantillon` varchar(50) DEFAULT NULL,
+  `Libele` varchar(50) DEFAULT NULL,
+  `QuantiteEchantillon` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `echantillon`
+--
+
+INSERT INTO `echantillon` (`Id_Echantillon`, `DateDistributionEchantillon`, `NomEchantillon`, `Libele`, `QuantiteEchantillon`) VALUES
+(1, '2025-04-06', 'echantillon 1', 'libelle ech 1', '5'),
+(2, '2025-03-26', 'echantillon 2', 'libelle ech 2', '7');
 
 -- --------------------------------------------------------
 
@@ -73,19 +49,16 @@ CREATE TABLE IF NOT EXISTS `echantillon` (
 -- Structure de la table `practicien`
 --
 
-DROP TABLE IF EXISTS `practicien`;
-CREATE TABLE IF NOT EXISTS `practicien` (
-  `Id_Practicien` int NOT NULL AUTO_INCREMENT,
-  `EmailPracticien` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `SpecialiteMedecin` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `DescriptionMedecin` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `Cabinet` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `AdressePracticien` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `CodePostalPracticien` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `VillePracticien` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `IdRegion` int NOT NULL,
-  PRIMARY KEY (`Id_Practicien`),
-  KEY `IdRegion` (`IdRegion`)
+CREATE TABLE `practicien` (
+  `Id_Practicien` int(11) NOT NULL,
+  `EmailPracticien` varchar(50) DEFAULT NULL,
+  `SpecialiteMedecin` varchar(50) DEFAULT NULL,
+  `DescriptionMedecin` varchar(255) DEFAULT NULL,
+  `Cabinet` varchar(255) DEFAULT NULL,
+  `AdressePracticien` varchar(50) DEFAULT NULL,
+  `CodePostalPracticien` varchar(50) DEFAULT NULL,
+  `VillePracticien` varchar(50) DEFAULT NULL,
+  `IdRegion` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -94,15 +67,21 @@ CREATE TABLE IF NOT EXISTS `practicien` (
 -- Structure de la table `produit`
 --
 
-DROP TABLE IF EXISTS `produit`;
-CREATE TABLE IF NOT EXISTS `produit` (
-  `Id_Produit` int NOT NULL AUTO_INCREMENT,
-  `NomProduit` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+CREATE TABLE `produit` (
+  `Id_Produit` int(11) NOT NULL,
+  `NomProduit` varchar(50) DEFAULT NULL,
   `DateAjoutProduit` date DEFAULT NULL,
-  `Libele` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `QuantiteProduit` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`Id_Produit`)
+  `Libele` varchar(50) DEFAULT NULL,
+  `QuantiteProduit` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`Id_Produit`, `NomProduit`, `DateAjoutProduit`, `Libele`, `QuantiteProduit`) VALUES
+(1, 'produit 1', '2025-04-07', 'libelle 1', '5'),
+(2, 'produit 2', '2025-04-06', 'libelle 2', '7');
 
 -- --------------------------------------------------------
 
@@ -110,21 +89,24 @@ CREATE TABLE IF NOT EXISTS `produit` (
 -- Structure de la table `rapport`
 --
 
-DROP TABLE IF EXISTS `rapport`;
-CREATE TABLE IF NOT EXISTS `rapport` (
-  `Id_Rappport` int NOT NULL AUTO_INCREMENT,
-  `AdresseRapport` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+CREATE TABLE `rapport` (
+  `Id_Rappport` int(11) NOT NULL,
+  `AdresseRapport` varchar(255) DEFAULT NULL,
   `DateRapport` date DEFAULT NULL,
-  `Id_Echantillon` int NOT NULL,
-  `Id_Produit` int NOT NULL,
-  `Id_Visiteur` int NOT NULL,
-  `Id_Practicien` int NOT NULL,
-  PRIMARY KEY (`Id_Rappport`),
-  KEY `Id_Echantillon` (`Id_Echantillon`),
-  KEY `Id_Produit` (`Id_Produit`),
-  KEY `Id_Visiteur` (`Id_Visiteur`),
-  KEY `Id_Practicien` (`Id_Practicien`)
+  `CodePostal` int(5) DEFAULT NULL,
+  `Id_Echantillon` int(11) NOT NULL,
+  `Id_Produit` int(11) NOT NULL,
+  `Id_Visiteur` int(11) NOT NULL,
+  `Id_Practicien` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `rapport`
+--
+
+INSERT INTO `rapport` (`Id_Rappport`, `AdresseRapport`, `DateRapport`, `CodePostal`, `Id_Echantillon`, `Id_Produit`, `Id_Visiteur`, `Id_Practicien`) VALUES
+(1, '25 avenue de Montlouis 69410 Champagne-au-Mont-d\'Or', '2025-04-08', 0, 0, 0, 0, 1),
+(2, '25 avenue de Montlouis 69410 Champagne-au-Mont-d\'Or', '2025-04-08', 0, 0, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -132,27 +114,34 @@ CREATE TABLE IF NOT EXISTS `rapport` (
 -- Structure de la table `region`
 --
 
-DROP TABLE IF EXISTS `region`;
-CREATE TABLE IF NOT EXISTS `region` (
-  `IdRegion` int NOT NULL AUTO_INCREMENT,
-  `CodePostal` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NomRegion` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`IdRegion`)
+CREATE TABLE `region` (
+  `IdRegion` int(11) NOT NULL,
+  `NomRegion` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `responsable`
+-- Déchargement des données de la table `region`
 --
 
-DROP TABLE IF EXISTS `responsable`;
-CREATE TABLE IF NOT EXISTS `responsable` (
-  `Id_Responsable` int NOT NULL AUTO_INCREMENT,
-  `Id_Utilisateur` int NOT NULL,
-  PRIMARY KEY (`Id_Responsable`),
-  KEY `Id_Utilisateur` (`Id_Utilisateur`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `region` (`IdRegion`, `NomRegion`) VALUES
+(3, 'Auvergne-Rhône-Alpes'),
+(4, 'Bourgogne-Franche-Comté'),
+(5, 'Bretagne'),
+(6, 'Centre-Val de Loire'),
+(7, 'Corse'),
+(8, 'Grand Est'),
+(9, 'Hauts-de-France'),
+(10, 'Île-de-France'),
+(11, 'Normandie'),
+(12, 'Nouvelle-Aquitaine'),
+(13, 'Occitanie'),
+(14, 'Pays de la Loire'),
+(15, 'Provence-Alpes-Côte d\'Azur'),
+(16, 'Guadeloupe'),
+(17, 'Martinique'),
+(18, 'Guyane'),
+(19, 'La Réunion'),
+(20, 'Mayotte');
 
 -- --------------------------------------------------------
 
@@ -160,43 +149,102 @@ CREATE TABLE IF NOT EXISTS `responsable` (
 -- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `Id_Utilisateur` int NOT NULL AUTO_INCREMENT,
-  `MotDePasse` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NomUtilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `PrenomUtilisateur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `Login` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NumeroTelephoneUtilisateur` decimal(10,2) DEFAULT NULL,
-  `MailUtilisateur` decimal(10,2) DEFAULT NULL,
-  `Rôle` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`Id_Utilisateur`)
+CREATE TABLE `utilisateur` (
+  `Id_Utilisateur` int(11) NOT NULL,
+  `MotDePasse` varchar(255) DEFAULT NULL,
+  `NomUtilisateur` varchar(50) DEFAULT NULL,
+  `Role` enum('visiteur','delegue','responsable') NOT NULL,
+  `PrenomUtilisateur` varchar(50) DEFAULT NULL,
+  `NumeroTelephoneUtilisateur` varchar(15) DEFAULT NULL,
+  `MailUtilisateur` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `visiteur`
+-- Index pour les tables déchargées
 --
 
-DROP TABLE IF EXISTS `visiteur`;
-CREATE TABLE IF NOT EXISTS `visiteur` (
-  `Id_Visiteur` int NOT NULL AUTO_INCREMENT,
-  `NumeroTelephoneVisiteur` decimal(10,2) DEFAULT NULL,
-  `PrenomVisiteur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `AdresseVisiteur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `MailVisiteur` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `IdRegion` int NOT NULL,
-  `Id_Délégué` int NOT NULL,
-  `Id_Utilisateur` int NOT NULL,
-  PRIMARY KEY (`Id_Visiteur`),
-  KEY `IdRegion` (`IdRegion`),
-  KEY `Id_Délégué` (`Id_Délégué`),
-  KEY `Id_Utilisateur` (`Id_Utilisateur`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Index pour la table `echantillon`
+--
+ALTER TABLE `echantillon`
+  ADD PRIMARY KEY (`Id_Echantillon`);
+
+--
+-- Index pour la table `practicien`
+--
+ALTER TABLE `practicien`
+  ADD PRIMARY KEY (`Id_Practicien`),
+  ADD KEY `IdRegion` (`IdRegion`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`Id_Produit`);
+
+--
+-- Index pour la table `rapport`
+--
+ALTER TABLE `rapport`
+  ADD PRIMARY KEY (`Id_Rappport`),
+  ADD KEY `Id_Echantillon` (`Id_Echantillon`),
+  ADD KEY `Id_Produit` (`Id_Produit`),
+  ADD KEY `Id_Visiteur` (`Id_Visiteur`),
+  ADD KEY `Id_Practicien` (`Id_Practicien`);
+
+--
+-- Index pour la table `region`
+--
+ALTER TABLE `region`
+  ADD PRIMARY KEY (`IdRegion`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`Id_Utilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `echantillon`
+--
+ALTER TABLE `echantillon`
+  MODIFY `Id_Echantillon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `practicien`
+--
+ALTER TABLE `practicien`
+  MODIFY `Id_Practicien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `Id_Produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `rapport`
+--
+ALTER TABLE `rapport`
+  MODIFY `Id_Rappport` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `region`
+--
+ALTER TABLE `region`
+  MODIFY `IdRegion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `Id_Utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
